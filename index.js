f45505c4a84d811598f5d4591782383a0819d98b
donpierone <info@donpiero.it>
2025-03-07 15:32:09 +0100

    Aggiunto server Express.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Don Piero!");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Benvenuto su Don Piero! Il server è attivo.");
+});
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+});
